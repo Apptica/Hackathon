95466301d12f675f6eee16f1b651638f3a814619
Asutosh Rana <asutoshrana97@gmail.com>
2018-03-09 13:07:23 +0530

    changed speech_main from py2 to py3

--- speech_tone/speech_main.py
+++ speech_tone/speech_main.py
@@ -5,7 +5,7 @@ import json
 data = speech_google.main()
 
 #data = '''Welcome to the IBM Hackathon hosted in IIIT Jabalpur and this is your host joining you to give live updates about the ongoings here. The participants here are looking very excited about the contest.'''
-print "\n\nFinal Output: \""+data + "\"\n\n"
+print("\n\nFinal Output: \""+data + "\"\n\n")
 
 tones = ''
 tone_json = json.loads(tone_analyser.tone(data))
@@ -15,8 +15,8 @@ if tone_json.has_key('document_tone') :
 				tones += ('\t' + res['tone_name'] + ' - ' + str(res['score']*100) + ' %\n');
 
 if not tones == ''  :
-	print 'Tones Detected:: \n' + tones
+	print('Tones Detected:: \n' + tones)
 else:
-	print 'No Tones Detected.\n'
+	print('No Tones Detected.\n')
 #parsed = json.loads(tone_json)
 #print json.dumps(parsed, indent=4, sort_keys=True)
