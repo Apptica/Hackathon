1c2ba6e5ccbc29d514f00c6246e221793f4aa0d7
Asutosh Rana <asutoshrana97@gmail.com>
2018-03-09 18:22:37 +0530

    output to file

--- speech_tone/speech_main.py
+++ speech_tone/speech_main.py
@@ -2,12 +2,17 @@ import tone_analyser
 import speech_google
 import json
 
-data = speech_google.main()
+FINAL_OUTPUT_FILE = "final_output.txt"
+TONE_FILE = "tone_output.txt"
 
-#data = '''Welcome to the IBM Hackathon hosted in IIIT Jabalpur and this is your host joining you to give live updates about the ongoings here. The participants here are looking very excited about the contest.'''
+#data = speech_google.main()
+
+data = '''Welcome to the IBM Hackathon hosted in IIIT Jabalpur and this is your host joining you to give live updates about the ongoings here. The participants here are looking very excited about the contest.'''
 
 print("\n\nFinal Output: \""+data + "\"\n\n")
-
+file = open(FINAL_OUTPUT_FILE, "w")
+file.write(data)
+file.close()
 
 tones = ''
 tone_json = json.loads(tone_analyser.tone(data))
@@ -16,10 +21,13 @@ if tone_json.has_key('document_tone') :
 		for res in tone_json['document_tone']['tones']:
 				tones += ('\t' + res['tone_name'] + ' - ' + str(res['score']*100) + ' %\n');
 
+file = open(TONE_FILE, "w")
 if not tones == ''  :
 	print('Tones Detected:: \n' + tones)
+	file.write('Tones Detected:: \n' + tones)
 else:
 	print('No Tones Detected.\n')
-
+	file.write('No Tones Detected.\n')
+file.close()
 #parsed = json.loads(tone_json)
 #print json.dumps(parsed, indent=4, sort_keys=True)
